27ecc3870339986697b8121030062b334604be1a
Harshit Chauhan <harshitchauhan988@gmail.com>
2025-12-09 10:32:50 +0530

    utils file added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import express from "express";
+import app from "./app.js";
 import connectDB from "./db/db.js";
-const app=express();
+
 import dotenv from "dotenv"; 
 dotenv.config();
-connectDB()
+
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`server is running at ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongo db error is ",err);
+})
